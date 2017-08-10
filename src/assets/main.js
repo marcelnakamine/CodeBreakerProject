d5478448bb8062357a27cb86b4057457e5e8ee47
Marcel Nakamine <marcel.nakamine@gmail.com>
2017-08-09 21:02:54 -0300

    generate random number between 0 and 9999

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,3 +7,12 @@ function guess() {
 }
 
 //implement new functions here
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function setHiddenFields() {
+  answer.value = getRandomInt(0,9999);
+}
