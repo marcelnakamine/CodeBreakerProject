e09f58365fe9edd049737aa548119eed29d4fc9d
Marcel Nakamine <marcel.nakamine@gmail.com>
2017-08-09 23:01:09 -0300

    add showAnswer and showReplay functions on guess function

--- src/assets/main.js
+++ src/assets/main.js
@@ -15,8 +15,12 @@ function guess() {
 
     if (getResults(input)) {
       setMessage("You Win! :)");
+      showAnswer(true);
+      showReplay();
     } else if (attempt.value >= 10) {
       setMessage("You Lose! :(");
+      showAnswer(false);
+      showReplay();
     } else {
       setMessage("Incorrect, try again.");
     }
